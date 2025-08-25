70f626e45c376344067941a8d713b01025767791
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-25 17:40:49 -0600

    Estructura basica mensajes
    ciclo while para dar a conocer las opciones

--- main.py
+++ main.py
@@ -5,5 +5,23 @@ from Menutexto import Menu
 txt = Menu()
 
 
-#>Menu Principal
-txt.MenuPrincipal()
+while True:
+    try:
+        #>Menu Principal
+        txt.MenuPrincipal()
+        #> Opción ingresada
+        ingreseopcion = input("Ingrese una opción: ")
+
+        #Opicones
+        if ingreseopcion == "1":
+            print("Opción 1")
+        elif ingreseopcion == "2":
+            print("Opción 2")
+        elif ingreseopcion == "9":
+            print("Saliendo del programa...")
+            break
+        
+        print("_"*25)
+        print()
+    except Exception:
+        print("Ingrese una opción válida.")
