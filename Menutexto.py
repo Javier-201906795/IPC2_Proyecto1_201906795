caab61d0a9b80a67fe4d3bb95a450a39a6386056
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-25 17:57:37 -0600

    Menu programa
    usando los requerimiento del enunciado

--- Menutexto.py
+++ Menutexto.py
@@ -5,7 +5,14 @@ class Menu():
 
     
     def MenuPrincipal(self):
-        print("*"*24)
-        print("*"+" "*2 + " Sistema DataAgro " + " "*2 + "*")
-        print("*"*24)
-        
+        print("*"*36)
+        print("*"+" "*8 + " Sistema DataAgro " + " "*8 + "*")
+        print("*"*36)
+        print("* 1."+" Cargar archivo " + " "*15 + "*")
+        print("* 2."+" Procesar archivo " + " "*13 + "*")
+        print("* 3."+" Escribir archivos salida" + " "*6 + "*")
+        print("* 4."+" Mostrar datos del estudiante " + " "*1 + "*")
+        print("* 5."+" Generar gráfica " + " "*14 + "*")
+        print("* 6."+" Salida " + " "*23 + "*")
+        print("*"*36)
+        print()
--- main.py
+++ main.py
@@ -5,26 +5,28 @@ from Menutexto import Menu
 txt = Menu()
 
 
+print("Proyecto IPC2")
+print("Javier Ricardo Yllescas Barrios")
+print("201906795")
+
 while True:
     try:
-        print("Proyecto IPC2")
-        print("Javier Ricardo Yllescas Barrios")
-        print("201906795")
+        
         #>Menu Principal
         txt.MenuPrincipal()
         #> Opción ingresada
-        ingreseopcion = input("Ingrese una opción: ")
+        ingreseopcion = int(input("Ingrese una opción: "))
 
         #Opicones
-        if ingreseopcion == "1":
+        if ingreseopcion == 1:
             print("Opción 1")
-        elif ingreseopcion == "2":
+        elif ingreseopcion == 2:
             print("Opción 2")
-        elif ingreseopcion == "9":
-            print("Saliendo del programa...")
+        elif ingreseopcion == 6:
+            print(">> Saliendo del programa...")
             break
         
         print("_"*25)
         print()
     except Exception:
-        print("Ingrese una opción válida.")
+        print("¡¡ Ingrese una opción válida. !!")
