410224074bab8f6ccd4b63635ac85f01772d739c
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-25 22:10:45 -0600

    Ruta TESTEO para no ingresar la ruta todo el tiempo
    Se agre una linea de codigo que coloca la ruta por defecto para poder testear el archivo con mas facilidad

--- SistemaCentral.py
+++ SistemaCentral.py
@@ -8,9 +8,12 @@ class Sistema:
         print(rutacompleta)
         #Leer archivo
         SA = SistemArchivos()
+        #Ruta para TESTEOS
+        rutacompleta = "G:\\2020\\2020_USAC\\Semestre14(2025)\\IPC2\\1_Laboratorio\\4_PROYECTO1\\IPC2_Proyecto1_201906795\\archivoejemplo.xml"
+        print(rutacompleta)
         #Recivir lectura
         campos = SA.leerArchivo(rutacompleta)
         print(campos)
-        
+
 
         
