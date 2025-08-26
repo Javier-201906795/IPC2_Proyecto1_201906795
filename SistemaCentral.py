1f90028b840384ea43dbc00a7353e9c9c56c3bb0
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-25 22:07:05 -0600

    Update SistemaCentral.py

--- SistemaCentral.py
+++ SistemaCentral.py
@@ -6,8 +6,11 @@ class Sistema:
     def CargarArchivos(self, rutacompleta):
         print(">> Cargando archivos...")
         print(rutacompleta)
+        #Leer archivo
         SA = SistemArchivos()
+        #Recivir lectura
         campos = SA.leerArchivo(rutacompleta)
         print(campos)
+        
 
         
