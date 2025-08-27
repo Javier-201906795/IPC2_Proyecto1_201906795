7a2a5f8e741ea8360da2550ea58ce437b4de2b99
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-27 08:19:59 -0600

    agregar InfoNodo()
    agrega funciones como desplegar muestra la informacion y esIgualALlave que valida si el id dado coincide

--- EstacionyFrecuencias.py
+++ EstacionyFrecuencias.py
@@ -1,11 +1,39 @@
 
+class InfoNodo():
+    
+    def desplegar():
+        pass
+    
+    def EsIgualALLave():
+        pass
 
-class Estacion:
+
+############################################################
+
+
+class Estacion(InfoNodo):
     def __init__(self, id, nombre):
         self.idE = id
         self.nombreE = nombre
+    
+    def desplegar(self):
+        print(f'Estacion_ID: {self.idE}  |  Estacion_Nombre: {self.nombreE}')
+    
+    def EsIgualALLave(self, id):
+        return self.idE == id
+    
 
-class Frecuencia:
+
+############################################################
+
+
+class Frecuencia(InfoNodo):
     def __init__(self, id, valor):
         self.idF = id
         self.valorF = int(valor.strip())
+    
+    def desplegar(self):
+        print(f'Frecuencia_ID: {self.idF}  |  Frecuencia_Valor: {self.valorF}')
+
+    def EsIgualALLave(self):
+        return self.idF == id
