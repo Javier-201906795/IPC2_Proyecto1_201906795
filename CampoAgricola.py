9fbd4674c3232b1219b5d879bab8a91f94238fcc
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-08-28 12:40:22 -0600

    obtener valor y convertirlo a numero

--- CampoAgricola.py
+++ CampoAgricola.py
@@ -135,23 +135,22 @@ class CampoAgricola(InfoNodo):
             #>Cultivo
             max_columna = self.matriz_cultivo.numero_columnas
             max_fila = self.matriz_cultivo.numero_filas
-            #cilo
+            print("-"*5+"[Matriz Cultivo]")
             for i in range(max_columna):
                 for j in range(max_fila):
                     print(f'[{i},{j}]')
                     self.matriz_cultivo.datocasilla(i,j).desplegar()
+                    #Obtener Valor casilla
+                    valor = self.matriz_cultivo.datocasilla(i,j).valor
+                    #Convertir a numero
+                    try:
+                        valornumero = int(valor)
+                    except:
+                        print(f"¡¡¡ No se pudo convertir en valor la casilla [{i},{j}] !!!")
+                    print(valornumero)
             
-            print("-"*5+"datos, Suelo")
-            #>Suelo
-            max_columna = self.matriz_suelo.numero_columnas
-            max_fila = self.matriz_suelo.numero_filas
-            #cilo
-            for i in range(max_columna):
-                for j in range(max_fila):
-                    print(f'[{i},{j}]')
-                    self.matriz_suelo.datocasilla(i,j).desplegar()
             
-            print(f'Columnas: {max_columna}  Filas: {max_fila}')
+            
             
 
 
